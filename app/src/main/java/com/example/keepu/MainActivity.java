38f25d21f19c073f5154403857ccb99dcb30b091
David Gravett <davidegravett@gmail.com>
2019-04-01 00:16:54 -0400

    update color

--- app/src/main/java/com/example/keepu/MainActivity.java
+++ app/src/main/java/com/example/keepu/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +19,17 @@ public class MainActivity extends AppCompatActivity {
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                goToSecondActivity();
+                EditText textInputUsername = findViewById(R.id.textInputUsername);
+                String usernameInput = textInputUsername.getText().toString();
+                EditText textInputPassword = findViewById(R.id.textInputPassword);
+                String passwordInput = textInputPassword.getText().toString();
 
+                if(usernameInput.equals("admin") && passwordInput.equals("password")){
+                    goToSecondActivity();
+                }else{
+                    TextView incorrectLogin = findViewById(R.id.textViewIncorrectLogin);
+                    incorrectLogin.setVisibility(View.VISIBLE);
+                }
             }
 
         });
